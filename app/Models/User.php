56bb14be62e14c4178c965808ff2a801f9120275
3mcode-solutions <admin@3mcode-solutions.com>
2025-12-15 03:01:13 +0200

    feat: create User model with authentication, user details, relationships, and avatar URL accessor.

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,11 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function permissionRequests()
+    {
+        return $this->hasMany(PermissionRequest::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
